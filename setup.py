56ca055459f7419e67624eacb3510bbdde0a6e1b
nixbytes <real8686@gmail.com>
2020-10-26 21:06:17 -0400

    add setup for packages

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='PyVaders',
+    version='2020.10.26',
+    author='nixbytes',
+    author_email='real8686@gmail.com',
+    description='pyvaders is classic game of shooting the enemies by a Space ship',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nixbytes/pyvaders',
+    packages=find_packages('src')
+)
