ce7225f3e5d7e6b7b9301d8658b6313ee280d201
nixbytes <real8686@gmail.com>
2020-10-30 22:30:03 -0400

    add screen fill test

--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ import pygame
 # intial instances of pygame
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 
-# Title Logo 
+# Title Logo
 
 pygame.display.set_caption("PyVaders")
-icon = pygame.image.load('alien.png')
+icon = pygame.image.load("alien.png")
 pygame.display.set_icon(icon)
 
 # Game Loop
@@ -19,3 +19,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Draw with the following colors
+    screen.fill((5, 0, 0))
+    pygame.display.update()
