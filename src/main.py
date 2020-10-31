7f37d009a5cf7099d9fa611f311f0ff6e2c76c4e
nixbytes <real8686@gmail.com>
2020-10-31 15:55:30 -0400

    add keyboard function

--- src/main.py
+++ src/main.py
@@ -16,10 +16,11 @@ pygame.display.set_icon(icon)
 player_image = pygame.image.load("spaceship.png")
 player_x = 370
 player_y = 480
+player_x_change = 0
 
 
-def player():
-    screen.blit(player_image, (player_x, player_y))
+def player(x, y):
+    screen.blit(player_image, (x, y))
 
 
 # Game Loop
@@ -27,11 +28,27 @@ def player():
 running = True
 
 while running:
+
+    screen.fill((5, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_x_change = -0.1
+                print("Left Key is press")
+            if event.key == pygame.K_RIGHT:
+                player_x_change = 0.1
+                print("Right Key is press")
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_x_change = 0
+
     # Draw with the following colors
     # screen.fill((5, 0, 0))
-    player()
+    player_x += player_x_change
+    player(player_x, player_y)
     pygame.display.update()
